b5052103ab0ed1f3ab68ae68fc427563734c629c
Jake Shilling <shilling.jake@gmail.com>
2023-05-25 20:39:09 -0400

    Add an example clog project

---
+++ example/app.lisp
@@ -0,0 +1,19 @@
+(defpackage :cleact.example
+  (:use #:cl #:clog #:clog-gui #:cleact)
+  (:export start-app
+           on-new-window))
+
+(in-package :cleact.example)
+
+(defun app ()
+  (cleact.core:create-element :h1 nil "Hello, World"))
+
+(defun on-new-window (body)
+  (render (app) body))
+
+(defun start-app ()
+  (initialize
+   'on-new-window
+   :static-root
+   "./www/")
+  (open-browser))
---
+++ example/cleact-example.asd
@@ -0,0 +1,4 @@
+(asdf:defsystem "cleact-example"
+  :depends-on (:clog
+               :cleact)
+  :components ((:file "app.lisp ")))
